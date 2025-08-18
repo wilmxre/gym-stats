4b386c1fcee1f2fc936ab7d9395e1a1b45682759
Kocsis Laszlo <kocsis.laszlo@kadeno.ro>
2025-08-18 13:15:13 +0300

    Fix borderRadius property applications

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,11 +5,6 @@ export default {
   content: ['./src/**/*.{mjs,js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)'
-      },
       colors: {
         text: {
           50: 'hsl(var(--text-50))',
