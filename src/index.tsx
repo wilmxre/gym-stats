0b3de83ce6789c94b9a920fefae35e357278ae58
Kocsis Laszlo <kocsis.laszlo@kadeno.ro>
2025-08-18 11:44:04 +0300

    Create the initial project

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import App from '@/components/App'
+import { createRoot } from 'react-dom/client'
+import 'tailwindcss/tailwind.css'
+
+const container = document.getElementById('root') as HTMLDivElement
+const root = createRoot(container)
+
+root.render(<App />)
